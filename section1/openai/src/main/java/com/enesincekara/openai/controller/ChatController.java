844ca3dc2ba989868e667da57653b1534875c925
Enes Incekara <enesincekara61@gmail.com>
2026-09-04 00:04:14 +0300

    Added openai config and endpoint

--- section1/openai/src/main/java/com/enesincekara/openai/controller/ChatController.java
+++ section1/openai/src/main/java/com/enesincekara/openai/controller/ChatController.java
@@ -1,6 +1,7 @@
 package com.enesincekara.openai.controller;
 
 
+import org.springframework.ai.chat.client.ChatClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -10,9 +11,18 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api")
 public class ChatController {
 
+    private final ChatClient chatClient;
+    public ChatController(ChatClient.Builder chatClientBuilder) {
+        this.chatClient = chatClientBuilder.build();
+    }
+
     @GetMapping("/chat")
     public String chat(@RequestParam("message") String message) {
-        return "Hello " + message;
+
+        return chatClient
+                .prompt(message)
+                .call()
+                .content();
     }
 
 }
